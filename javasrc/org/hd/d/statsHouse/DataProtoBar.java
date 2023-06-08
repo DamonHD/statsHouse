18299b6d3c4e6779c013d46c1518ec73be6e7222
DamonHD <d@hd.org>
2023-06-08 20:55:09 +0100

    Tidy-up

--- javasrc/org/hd/d/statsHouse/DataProtoBar.java
+++ javasrc/org/hd/d/statsHouse/DataProtoBar.java
@@ -4,13 +4,17 @@ import java.util.Objects;
 
 import org.hd.d.statsHouse.DataUtils.EOUDataCSV;
 
-/**A prototype music bar made from (a subset of) raw EOUDataCSV records.
+/**A prototype music bar made from (a subset of) raw EOUDataCSV records; immutable.
  *
  * Entries in dataRows may be null (padding at either end)
  * or contiguous in-date-order EOUDataCSV rows.
  * <p>
  * This can be used as an intermediate data representation
  * between the raw data coming in and the music bars.
+ * <p>
+ * For full immutability, this relies on the incoming
+ * leaf List values being immutable.  They must also
+ * be well-formed, eg non-null, in order, consistent date cadence.
  * 
  * @param dataNotesPerBar number of data/note slots; strictly positive
  * @param dataRows must be exactly dataNotesPerBar long; never null
@@ -19,9 +23,9 @@ public record DataProtoBar(int dataNotesPerBar, EOUDataCSV dataRows)
     {
 	public DataProtoBar
 		{
-		if(dataNotesPerBar < 1) { throw new IllegalArgumentException(); }
 		Objects.requireNonNull(dataRows);
 		Objects.requireNonNull(dataRows.data());
+		if(dataNotesPerBar < 1) { throw new IllegalArgumentException(); }
 		if(dataNotesPerBar != dataRows.data().size()) { throw new IllegalArgumentException(); }
 		}
     }
