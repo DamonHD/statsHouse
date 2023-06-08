e8450444525abd87a5d8e22d0dd9a247af8bc67e
DamonHD <d@hd.org>
2023-06-08 14:47:24 +0100

    Biggerer test.

--- test/javasrc/localtest/TestDataCSVRead.java
+++ test/javasrc/localtest/TestDataCSVRead.java
@@ -31,7 +31,6 @@ public final class TestDataCSVRead extends TestCase
     /**Verify that read of empty CSV works. */
     public static void testReadEmpty() throws IOException
 	    {
-        // Parse without checking the header text (ie data type)...
         final List<List<String>> result1 = DataUtils.parseEOUDataCSV(new StringReader(""));
         assertTrue("0 bytes should be empty", result1.isEmpty());
         
@@ -40,6 +39,38 @@ public final class TestDataCSVRead extends TestCase
 
         final List<List<String>> result3 = DataUtils.parseEOUDataCSV(new StringReader("#comment\r\n"));
         assertTrue("Comment should be empty", result3.isEmpty());
+        
+        
 	    }
 
+    /**Verify that read of sample CSV works.
+     * Sample from:
+     * <pre>
+% head data/consolidated/energy/std/gen/M/gen-M.csv
+     * </pre>
+     * @throws IOException
+     */
+    public static void testReadSampple() throws IOException
+	    {
+        final String sample = """
+#YYYY-MM,device,coverage,gen,device,coverage,gen,device,coverage,gen
+#input,"data/consolidated/energy/std/gen/M/Enphase/gen-M-Enphase.csv"
+#input,"data/consolidated/energy/std/gen/M/meter/gen-M-meter.csv"
+#input,"data/consolidated/energy/std/gen/M/SunnyBeam/gen-M-SunnyBeam.csv"
+2008-02,,,,meter,1,4,SunnyBeam,0.142857,3.54
+2008-03,,,,meter,1,70,SunnyBeam,1,68.55
+2008-04,,,,meter,1,108,SunnyBeam,1,106.13
+2008-05,,,,meter,1,135,SunnyBeam,1,134.03
+2008-06,,,,meter,1,160,SunnyBeam,1,158.1
+2008-07,,,,meter,1,161,SunnyBeam,1,146.12
+""";
+
+        final List<List<String>> result1 = DataUtils.parseEOUDataCSV(new StringReader(sample));
+        assertEquals("Sample should have 6 data rows", 6, result1.size());
+
+
+        // TODO
+
+
+	    }
     }
