d388b54ac5392fb397d705c0b7871c1c6ffc0503
DamonHD <d@hd.org>
2023-06-10 13:50:04 +0100

    Added MIDIGen.

---
+++ javasrc/org/hd/d/statsHouse/MIDIGen.java
@@ -0,0 +1,29 @@
+/*
+Copyright (c) 2023, Damon Hart-Davis
+
+Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package org.hd.d.statsHouse;
+
+/**MIDI generation.
+ * Initially will generate CSV suitable as input for the MIDICSV utility.
+ * May also generate MIDI and/or audio output directly with the javax.midi package.
+ */
+public final class MIDIGen
+    {
+
+
+
+
+    }
