ecb8aa4d365fee25378acfe1e9104c0922027a92
DamonHD <d@hd.org>
2023-06-12 19:56:36 +0100

    Morphed to MIDIPlayableMonophonicBar.

--- javasrc/org/hd/d/statsHouse/MIDIPlayableMonophonicBar.java
+++ javasrc/org/hd/d/statsHouse/MIDIPlayableMonophonicBar.java
@@ -1,13 +1,11 @@
 package org.hd.d.statsHouse;
 
 /**Immutable bar, playable as MIDI, eg by conversion to MIDICSV or adding to a Track within a Sequence.
- * Represents data melody and other content such as percussion.
+ * Represents data melody from one data stream for one playable bar.
  * <p>
- * Covers one voice/track, though can be polyphonic, eg for a percussion track.
+ * Covers one voice/track, can cannot be polyphonic.
  * <p>
  * Refers to source DataProtoBar and data stream number if data melody.
- * <p>
- * This can accommodate some MIDI meta events such as CC.
  * <p>
  * This does not contain an absolute time or bar number
  * to allow it to be used more than once in the same track,
@@ -15,7 +13,7 @@ package org.hd.d.statsHouse;
  * <p>
  * This may be the output of a chain of transformations.
  */
-public final class MIDIPlayableBar
+public final class MIDIPlayableMonophonicBar
     {
 
     }
