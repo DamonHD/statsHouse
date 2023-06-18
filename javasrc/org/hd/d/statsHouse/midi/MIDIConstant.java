00c385756e1383b10b7dad91ee9ecf7875d6277a
DamonHD <d@hd.org>
2023-06-18 08:43:49 +0100

    Added MIDIConstant.

---
+++ javasrc/org/hd/d/statsHouse/midi/MIDIConstant.java
@@ -0,0 +1,19 @@
+package org.hd.d.statsHouse.midi;
+
+/**Various MIDI constants. */
+public final class MIDIConstant
+	{
+    /**Prevent creation of an instance. */
+    private MIDIConstant() { }
+
+    /**GM1 generators must support at least 16 melodic voices at once. */
+    public static final int GM1_MIN_MELODIC_VOICES = 16;
+    /**GM1 generators must support at least 16 percussive voices at once. */
+    public static final int GM1_MIN_PERCUSSIVE_VOICES = 8;
+
+    /**GM1 generators must support at least 16 channels at once. */
+    public static final int GM1_MIN_CHANNELS = 16;
+
+    /**GM1 reserved percussion channel. */
+    public static final byte GM1_PERCUSSION_CHANNEL = 10;
+	}
