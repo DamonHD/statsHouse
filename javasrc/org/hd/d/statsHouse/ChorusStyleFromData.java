632277eb92f64616c5bc724cb42a5749198f18a6
DamonHD <d@hd.org>
2023-06-29 20:05:26 +0100

    WIP

--- javasrc/org/hd/d/statsHouse/ChorusStyleFromData.java
+++ javasrc/org/hd/d/statsHouse/ChorusStyleFromData.java
@@ -6,7 +6,7 @@ public enum ChorusStyleFromData
 	FirstDataBar,
     FirstFullDataBar,
     SyntheticRepresentativeDataBar,
-    SyntheticRepresentativeDataBarAndCounterpoint,
+    SyntheticRepresentativeDataBarPlusCounterpoint,
     MeansPlusSyntheticRepresentativeDataBar,
-    MeansPlusSyntheticRepresentativeDataBarAndCounterpoint;
+    MeansPlusSyntheticRepresentativeDataBarPlusCounterpoint;
 	}
