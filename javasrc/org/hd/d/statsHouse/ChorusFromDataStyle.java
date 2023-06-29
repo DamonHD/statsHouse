e5d2fec83157622dbe907c401b90ddfa14c80a1e
DamonHD <d@hd.org>
2023-06-29 18:51:31 +0100

    Adding ChorusFromDataStyle.

---
+++ javasrc/org/hd/d/statsHouse/ChorusFromDataStyle.java
@@ -0,0 +1,11 @@
+package org.hd.d.statsHouse;
+
+/**This enumerates some possible algorithmic styles of data melody chorus generation. */
+public enum ChorusFromDataStyle
+	{
+	FirstDataBar,
+    FirstFullDataBar,
+    SyntheticRepresentativeDataBar,
+    MeansPlusSyntheticRepresentativeDataBar,
+    MeansPlusSyntheticRepresentativeDataBarAndCounterpoint;
+	}
