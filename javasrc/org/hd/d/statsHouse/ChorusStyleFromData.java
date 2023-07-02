e604fed603b95f84769abcc20d3d7ac19e1e0ab3
DamonHD <d@hd.org>
2023-07-02 18:05:42 +0100

    comment

--- javasrc/org/hd/d/statsHouse/ChorusStyleFromData.java
+++ javasrc/org/hd/d/statsHouse/ChorusStyleFromData.java
@@ -3,14 +3,16 @@ package org.hd.d.statsHouse;
 /**This enumerates some possible algorithmic styles of data melody chorus generation. */
 public enum ChorusStyleFromData
 	{
+	/**Use the first data stream bar as-is; may be partly or entirely empty. */
 	FirstDataBar,
+	/**More robust for aligned data and/or on non-primary stream; such a full bar may not exist so will need fallback. */
     FirstFullDataBar,
 
-    /**Can capture means and max/min or 90/10 percentile points. */
+    /**Can capture means and max/min or 90/10 percentile points; hopes for periodicity on ls date component. */
     SyntheticRepresentativeDataBar,
     SyntheticRepresentativeDataBarPlusCounterpoint,
 
-    /**Downsampling in at least one bar, then synthetic style. */
+    /**Downsampling in at least one bar, then synthetic style; hopes for periodicity on ls date component. */
     MeansPlusSyntheticRepresentativeDataBar,
     MeansPlusSyntheticRepresentativeDataBarPlusCounterpoint;
 	}
