bce235f1f99ecc4f33c4ff4563e44c2008b0b9f3
DamonHD <d@hd.org>
2023-07-15 20:42:43 +0100

    Moar alignment tests.

--- test/javasrc/localtest/TestSplitAndAlign.java
+++ test/javasrc/localtest/TestSplitAndAlign.java
@@ -27,6 +27,7 @@ import javax.sound.midi.InvalidMidiDataException;
 import org.hd.d.statsHouse.GenerationParameters;
 import org.hd.d.statsHouse.data.DataProtoBar;
 import org.hd.d.statsHouse.data.EOUDataCSV;
+import org.hd.d.statsHouse.generic.DataCadence;
 import org.hd.d.statsHouse.generic.Style;
 import org.hd.d.statsHouse.generic.TuneSection;
 import org.hd.d.statsHouse.midi.MIDIGen;
@@ -60,7 +61,7 @@ public final class TestSplitAndAlign extends TestCase
 	    assertEquals("4 bars of 4 notes", 4, result2.size());
 	    assertEquals("4 bars of 4 notes", 4, result2.get(0).dataNotesPerBar());
 
-	    // Testing case with non-aligned data in a plain style, so should stay unaligned.
+	    // Testing short M sample with non-aligned data in a plain style, so should stay unaligned.
     	final List<DataProtoBar> result3 = MIDIGen.splitAndAlignData(TuneSection.verse,
 			new GenerationParameters(GenerationParameters.RANDOMNESS_NONE, Style.plain, 0, false, null),
 			EOUDataCSV.parseEOUDataCSV(new StringReader(BuiltInCSVDataSamples.sample_gen_M)));
@@ -72,7 +73,7 @@ public final class TestSplitAndAlign extends TestCase
 	    assertNotNull("first note in unaligned bar should not be null", result3.get(0).dataRows().data().get(0));
 	    assertNull("last note in bar should be null", result3.get(0).dataRows().data().get(11));
 
-	    // Testing case with non-aligned data in a gentle style, so should be aligned.
+	    // Testing short M sample with non-aligned data in a gentle style, so should be aligned.
     	final List<DataProtoBar> result4 = MIDIGen.splitAndAlignData(TuneSection.verse,
 			new GenerationParameters(GenerationParameters.RANDOMNESS_NONE, Style.gentle, 0, false, null),
 			EOUDataCSV.parseEOUDataCSV(new StringReader(BuiltInCSVDataSamples.sample_gen_M)));
@@ -84,7 +85,7 @@ public final class TestSplitAndAlign extends TestCase
 	    assertNull("first note in unaligned bar should be null", result4.get(0).dataRows().data().get(0));
 	    assertNull("last note in bar should be null", result4.get(0).dataRows().data().get(11));
 
-	    // Testing case with non-aligned data in a house style, so should be aligned.
+	    // Testing short M sample with non-aligned data in a house style, so should be aligned.
     	final List<DataProtoBar> result5 = MIDIGen.splitAndAlignData(TuneSection.verse,
 			new GenerationParameters(GenerationParameters.RANDOMNESS_NONE, Style.gentle, 0, false, null),
 			EOUDataCSV.parseEOUDataCSV(new StringReader(BuiltInCSVDataSamples.sample_gen_M)));
@@ -95,5 +96,17 @@ public final class TestSplitAndAlign extends TestCase
 	    assertEquals("1 bar of 6/12 notes", 12, result5.get(0).dataNotesPerBar());
 	    assertNull("first note in unaligned bar should be null", result5.get(0).dataRows().data().get(0));
 	    assertNull("last note in bar should be null", result5.get(0).dataRows().data().get(11));
+
+	    // Testing long M sample with aligned data in a plain style, so should stay unaligned.
+    	final List<DataProtoBar> result6 = MIDIGen.splitAndAlignData(TuneSection.verse,
+			new GenerationParameters(GenerationParameters.RANDOMNESS_NONE, Style.plain, 0, false, null),
+			EOUDataCSV.parseEOUDataCSV(new StringReader(BuiltInCSVDataSamples.conexDHW_M_to_202305)));
+	    assertNotNull(result6);
+	    assertFalse(result6.isEmpty());
+	    assertEquals("16 bars", 16, result6.size());
+	    assertEquals("first bar should have nominal 12 notes", DataCadence.M.defaultPerBar, result6.get(0).dataNotesPerBar());
+	    assertEquals("first bar should be full", 12, result6.get(0).dataRows().data().stream().filter(Objects::nonNull).count());
+	    assertNotNull("first note in unaligned first bar should not be null", result6.get(0).dataRows().data().get(0));
+	    assertNull("last note in last bar should be null", result6.get(15).dataRows().data().get(11));
 	    }
     }
