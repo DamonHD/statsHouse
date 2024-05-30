7d1ab15ba59eeefe8c186feac66760904a4fc578
DamonHD <d@hd.org>
2024-05-30 12:05:39 +0100

    WIP: Main command processing

--- javasrc/org/hd/d/statsHouse/feedHits/Main.java
+++ javasrc/org/hd/d/statsHouse/feedHits/Main.java
@@ -17,6 +17,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 package org.hd.d.statsHouse.feedHits;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -41,6 +42,9 @@ public final class Main
         System.err.println("Commands/options:");
         System.err.println("  -help");
         System.err.println("    This summary/help.");
+        System.err.println("  -@(<cmdfilename>|-)");
+        System.err.println("    Read independent command lines from specified file or stdin if '-'");
+        System.err.println("    Do not process further command-line arguments.");
         System.err.println("  -summary <typeN> {dir}*");
         System.err.println("    Summary/quick play, 0 (use internal) or more directories.");
         System.err.println("    Each directory should contain a standard set of data extract files.");
@@ -54,6 +58,10 @@ public final class Main
 	 * This is one or more spaces or tabs.
 	 */
 	public static final Pattern delimCmdStream = Pattern.compile("[ \t]+");
+
+	/**Default output MIDI filename. */
+	public static final File DEFAULT_OUT_MIDI = new File("out.midi");
+
 
     /**Accepts command-line invocation. */
     public static void main(final String[] args)
@@ -143,28 +151,27 @@ public final class Main
 	public static void runCommands(final List<List<String>> cmdlines, final boolean quiet)
 		{
 		// Execute command line(s) sequentially, aborting at any exception.
-		final int cmdCount = 0;
+		int cmdCount = 0;
 		for(final List<String> cmdline : cmdlines)
 			{
 			final int argCount = cmdline.size();
-			if(argCount < 2)
-			    { throw new IllegalArgumentException("too few arguments: at least input.csv and -play or output.csv or output.mid required"); }
+			if(argCount < 1) { continue; } // Skip empty commands.
 
-		    final String inputFileName = cmdline.get(0);
-		    final String outputFileName = cmdline.get(1);
+		    final File outputFileName = DEFAULT_OUT_MIDI;
 
 		    try {
+		    	for(final List<String> cmd : cmdlines)
+			    	{
+					System.out.println("INFO: command: " +
+					        (++cmdCount) + "/" + (cmdlines.size()) + ": " +
+					        Arrays.toString(cmdline.toArray()));
+			    	}
 
 
-//					else
-				    	{
-				    	throw new IllegalArgumentException("unrecognised output type/suffix: " + outputFileName);
-				    	}
 				}
 		    catch(final Exception e)
 		        {
 				e.printStackTrace();
-		    	throw new RuntimeException("failed processing input " + inputFileName, e);
 				}
 			}
 		}
