a65b16af93e4f35d52a256bbedff6ea0277660cd
DamonHD <d@hd.org>
2024-06-04 18:37:59 +0100

    WIP

--- javasrc/org/hd/d/statsHouse/data/DataVizBeatPoint.java
+++ javasrc/org/hd/d/statsHouse/data/DataVizBeatPoint.java
@@ -16,14 +16,17 @@ Licensed under the Apache License, Version 2.0 (the "License");
 
 package org.hd.d.statsHouse.data;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 /**Data Visualisation for a data point vector per simple (typically 4/4) beat.
  *
- * @param dataLabels  column labels for dataRendered (may contain nulls);
- *     may be null
+ * @param dataLabels  column labels for dataRendered;
+ *     may contain nulls, may be null
  * @param dataRendered  the set of key data as rendered in the tune,
  *     with the outer List usually one item per bar beat,
  *     and the inner list an ordered list of the key items rendered in that beat maybe normalised;
@@ -42,5 +45,34 @@ public record DataVizBeatPoint(
     	if(nColumns < 0) { throw new IllegalArgumentException(); }
     	if((null != dataLabels) && (nColumns != dataLabels.size())) { throw new IllegalArgumentException(); }
 	    if(null != dataLabels) { dataLabels = Collections.unmodifiableList(new ArrayList<>(dataLabels)); } // Defensive copy.
+	    }
+
+    /**Output placeholder character in place of empty label or potential separator for write(). */
+    public static final char PlaceholderChar = '_';
+
+    /**Write dataset to stream for other tools such as gnuplot to visualise/render.
+     * Each beat vector of points is written to a record in a single line.
+     * <p>
+     * The first line may optionally be labels if dataLabels is not null.
+     * <p>
+     * Values in a record may be separated by spaces or commas.
+     */
+    public void write(final Writer w, final boolean commaSep) throws IOException
+	    {
+        Objects.requireNonNull(w);
+        if(null != dataLabels)
+	        {
+	        // Write column headings.
+        	// Convert any commas and spaces to underscores to avoid ambiguity.
+        	// A null label becomes a single non-empty place-holder value.
+        	for(int c = 0; c < nColumns; ++c)
+	        	{
+	        	String label = dataLabels.get(c);
+	        	if(null == label) { label = "" + PlaceholderChar; }
+	        	else { label = label.replace(',', PlaceholderChar).replace(' ', PlaceholderChar); }
+                if(c > 0) { w.append(commaSep ? ',' : ' '); }
+	        	w.append(label);
+	        	}
+	        }
 	    }
 	}
